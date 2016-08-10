ce66b0e0be3bc3f280faa6da73ea8a55643992bb
vagrant <vagrant@precise32.(none)>
2016-08-09 22:31:59 -0700

    refeactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -66,24 +66,9 @@ Rails.application.routes.draw do
   #     end
   #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
+
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
 
